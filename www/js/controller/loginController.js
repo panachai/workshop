da41a06b25ee1a160c5b5d94c2c8d490bd48d6a1
Panachai <panachai.ny@gmail.com>
2017-10-22 15:29:41 +0700

    update array and popup

--- www/js/controller/loginController.js
+++ www/js/controller/loginController.js
@@ -2,9 +2,8 @@ angular.module('workshop')
 
   .controller('loginController', function ($scope, $state) {
     console.log('loginController Start...');
-    $scope.myName = 'Panachai';
 
-    $scope.btnAbout = function () {
+    $scope.btnLogin = function () {
       console.log('btnLogin pressed.');
       $state.go('list');
     }
